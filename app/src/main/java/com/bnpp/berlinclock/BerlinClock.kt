9548f8a4334a070cab74f1461464dfc11db27398
rajesh-udhayan <rajeshkudhayan@gmail.com>
2022-06-21 16:53:58 +0530

    Code refactoring for readability Extracted more than 5 minutes into a separate logic

--- app/src/main/java/com/bnpp/berlinclock/BerlinClock.kt
+++ app/src/main/java/com/bnpp/berlinclock/BerlinClock.kt
@@ -9,22 +9,23 @@ class BerlinClock {
         if (seconds % 2 == 0) YELLOW
         else OFF
 
-    fun getMinutes(minutes: Int): Minutes {
-        return when(minutes){
-            in 1..4 -> Minutes(
-                bottomLamps = getBottomLampsColor(minutes)
-            )
-            5 -> {
-                val topMinutes = listOf(YELLOW, OFF, OFF, OFF, OFF, OFF, OFF, OFF, OFF, OFF, OFF)
-                Minutes(topMinutes, bottomLamps = Minutes.defaultBottom())
-            }
-            6 -> {
-                val topMinutes = listOf(YELLOW, OFF, OFF, OFF, OFF, OFF, OFF, OFF, OFF, OFF, OFF)
-                val bottomMinutes = listOf(YELLOW, OFF, OFF, OFF)
-                Minutes(topMinutes, bottomMinutes)
-            }
+    fun getMinutes(minutes: Int): Minutes = when{
+            minutes.lessThanFive() -> getLampsForLessThanFiveMinutes(minutes)
+            minutes.greaterThanOrEqualsFive() -> getLampsForMoreThanFiveMinutes(minutes)
             else -> Minutes()
         }
+
+    private fun getLampsForLessThanFiveMinutes(minutes: Int) =
+        Minutes(
+            bottomLamps = getBottomLampsColor(minutes)
+        )
+
+    private fun getLampsForMoreThanFiveMinutes(minutes: Int): Minutes {
+        val topLampsCount = minutes/5
+        val bottomLampsCount = minutes%5
+        val topMinutes = getTopLampsColor(topLampsCount)
+        val bottomMinutes = getBottomLampsColor(bottomLampsCount)
+        return Minutes(topMinutes,bottomMinutes)
     }
 
     private fun getBottomLampsColor(minutes: Int): List<LampColor> {
@@ -34,5 +35,16 @@ class BerlinClock {
         }
         return lampColors
     }
+
+    private fun getTopLampsColor(lampsCount: Int): List<LampColor> {
+        val lampColors: MutableList<LampColor> = Minutes.defaultTop()
+        (1..lampsCount).forEach{ i ->
+            lampColors[i-1] = YELLOW
+        }
+        return lampColors
+    }
+
+    private fun Int.greaterThanOrEqualsFive() = this >= 5
+    private fun Int.lessThanFive() = this < 5
 
 }
