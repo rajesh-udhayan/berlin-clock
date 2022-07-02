1b9a3dd6e12bc0adb7045572c14d1abc674f2312
rajesh-udhayan <rajeshkudhayan@gmail.com>
2022-07-03 00:07:36 +0530

    Implemented timer for getting current time. Updating timer text with current time.

--- app/src/main/java/com/bnpp/berlinclock/BerlinClockActivity.kt
+++ app/src/main/java/com/bnpp/berlinclock/BerlinClockActivity.kt
@@ -1,6 +1,7 @@
 package com.bnpp.berlinclock
 
 import android.os.Bundle
+import android.os.CountDownTimer
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.background
@@ -9,12 +10,13 @@ import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.*
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.testTag
 import androidx.compose.ui.res.stringResource
-import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
@@ -24,11 +26,18 @@ import com.bnpp.berlinclock.ui.theme.BerlinClockTheme
 import com.bnpp.berlinclock.ui.theme.redEnabled
 import com.bnpp.berlinclock.ui.theme.yellowDisabled
 import com.bnpp.berlinclock.ui.theme.yellowEnabled
+import java.text.SimpleDateFormat
+import java.util.*
+
+private val currentTime = mutableStateOf("00:00:00")
 
 class BerlinClockActivity : ComponentActivity() {
 
+    private lateinit var timer: CountDownTimer
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        startTimer()
         setContent {
             BerlinClockTheme {
                 Surface(color = MaterialTheme.colors.background) {
@@ -36,6 +45,27 @@ class BerlinClockActivity : ComponentActivity() {
                 }
             }
         }
+    }
+
+    private fun startTimer() {
+        timer = object : CountDownTimer(COUNT_DOWN_MILLIS, COUNT_DOWN_INTERVAL) {
+            override fun onTick(millisUntilFinished: Long){
+                val mTime: String = SimpleDateFormat(TIME_FORMAT, Locale.getDefault()).format(
+                    Date()
+                )
+                currentTime.value = mTime
+            }
+            override fun onFinish() {
+                start()
+            }
+        }
+        timer.start()
+    }
+
+    companion object {
+        const val COUNT_DOWN_MILLIS = 600000L
+        const val COUNT_DOWN_INTERVAL = 1000L
+        const val TIME_FORMAT = "HH:mm:ss"
     }
 }
 
@@ -142,9 +172,10 @@ fun BerlinClockView() {
         }
 
         Spacer(modifier = Modifier.weight(1f))
+        val mTime by currentTime
         Text(
             modifier = Modifier.testTag("timeText"),
-            text = "11:59:59",
+            text = mTime,
             fontSize = 30.sp,
             color = Color.Black,
             fontWeight = FontWeight.Bold
